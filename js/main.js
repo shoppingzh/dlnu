2a0b8bd88b116f1c16989f27b46ef162ad8af5a4
shoppingzh <1049262057@qq.com>
2019-01-10 22:17:42 +0800

    配色选择与下拉菜单设计

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $('ul.nav>li>.nav-link').on({
+        mouseenter: function(){
+            $(this).siblings('.menus').addClass('open');
+        },
+        mouseleave: function(){
+            $(this).siblings('.menus').removeClass('open');
+        }
+    });
+    $('.menus').on({
+        mouseenter: function(){
+            $(this).addClass('open');
+        },
+        mouseleave: function(){
+            $(this).removeClass('open');
+        }
+    });
+
+
+});
